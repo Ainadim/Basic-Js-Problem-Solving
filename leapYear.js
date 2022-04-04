068679bdbed0d1fbf36df3252f5f10c821b1e7b1
Arafin Islam <nadimface@gmail.com>
2022-04-04 17:50:07 +0600

    3rd

---
+++ leapYear.js
@@ -0,0 +1,8 @@
+function isLeapYear(year) {
+    if ((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))) {
+        console.log(`${year} is a Leap year`)
+    } 
+    else {console.log(`${year} is not a Leap year`)}
+}
+
+isLeapYear(2024);
