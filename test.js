f4770957e2f845f56f0bed082c25d707e573da30
Arafin Islam <nadimface@gmail.com>
2022-04-05 20:07:24 +0600

    vowel  test

--- test.js
+++ test.js
@@ -47,11 +47,28 @@ leapYear (2024);
 
 
 
-const sentence = 'My name is Arafin and I am learning JavaScript'
+const vowel = 'aeiou'
+let count = 0;
 
-const allString = () => {
-    for (let i = 0; i < sentence.length; i++) {
-        const element = sentence[i];
-        console.log(element);        
+// function allstr(str) {
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowel.indexOf(str[i].toLowerCase ()) > -1) {
+//             count ++
+//         }
+//     }
+// }
+// allstr('My name is Arafin & I am learning JavaScript AEIOU');
+
+// console.log('There are total ' + count + ' vowel in the sentence');
+
+
+const allString = str => {
+    for (let i = 0; i < str.length; i++) {
+        if (vowel.indexOf(str[i].toLowerCase ()) > -1) {
+            count ++
+        }
     }
-}
+} 
+allString ('My name is Arafin & I am learning JavaScript AEIOU');
+
+console.log('There are total ' + count + ' vowel in the sentence');
